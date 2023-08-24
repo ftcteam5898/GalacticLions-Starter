50db519ad02ab8461e262257c91e8e4a07c346fc
Lara-Martins <94404267+Lara-Martins@users.noreply.github.com>
2023-08-24 09:15:21 -0400

    Add files via upload
    yippee

---
+++ TeamCode/src/main/StraferFieldCentric.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+/**
+ * This particular OpMode executes a field centric Teleop for a mecanum robot
+ * The code is structured as a LinearOpMode
+ *
+ * In this mode, the left stick moves the robot relative to the field.
+ * The Right stick rotates the robot left and right.
+ * The Logitech button resets the robot's heading.
+ *
+ */
+
+@TeleOp(name="Strafer - Field Centric", group="Starter Code")
+public class StraferFieldCentric extends LinearOpMode {
+
+    IMU imu;
+
+    @Override
+    public void runOpMode() {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("lf");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("lb");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("rf");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("rb");
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Retrieve the IMU from the hardware map
+        imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers or options on PS4-style controllers.
+            if (gamepad1.guide) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+        }
+    }
+}
