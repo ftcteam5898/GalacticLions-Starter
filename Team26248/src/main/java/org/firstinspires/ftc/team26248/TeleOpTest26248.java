665464f3361f4c44591ffd5855bbd205452ba630
JoeKudyba-LA <joe.kudyba@lakeviewacademy.org>
2024-11-16 09:33:24 -0500

    Update TeleOpTest26248.java

--- Team26248/src/main/java/org/firstinspires/ftc/team26248/TeleOpTest26248.java
+++ Team26248/src/main/java/org/firstinspires/ftc/team26248/TeleOpTest26248.java
@@ -189,7 +189,7 @@ public class TeleOpTest26248 extends OpMode {
         // slide control
         if (slideLimit)
         {
-            if (slidePos > -400 && gamepad2.left_stick_y < 0)
+            if (slidePos > -1300 && gamepad2.left_stick_y < 0)
                 slideMotor.setPower(gamepad2.left_stick_y * .5);
             else if (slidePos < -50 && gamepad2.left_stick_y > 0)
                 slideMotor.setPower(gamepad2.left_stick_y * .5);
