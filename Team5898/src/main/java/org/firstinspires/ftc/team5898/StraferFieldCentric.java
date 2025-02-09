0a4a205cad2c53e66f5ff73e4f6603baf18d07ab
mikhaillovell <34150685+mikhaillovell@users.noreply.github.com>
2025-02-08 19:49:15 -0500

    Disable old opmodes

--- Team5898/src/main/java/org/firstinspires/ftc/team5898/StraferFieldCentric.java
+++ Team5898/src/main/java/org/firstinspires/ftc/team5898/StraferFieldCentric.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.team5898;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +22,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
  *
  */
 
+@Disabled
 @TeleOp(name="Strafer - Field Centric", group="Starter Code")
 public class StraferFieldCentric extends LinearOpMode {
 
