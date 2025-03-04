d34f7c01083035dd73f426cf375465bd57be1ee8
Em Vick <em.vick@lakeviewacademy.org>
2025-03-04 15:29:33 -0500

    Create EmmasTeleOp.java

---
+++ Learning/src/main/java/org/firstinspires/ftc/learning/EmmasTeleOp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.learning;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Emma's TeleOp", group="Starter Code")
+public class EmmasTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+}
