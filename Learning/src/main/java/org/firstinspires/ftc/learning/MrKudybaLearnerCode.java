d992325b266b882310e478753317dd68a5b33515
JoeKudyba-LA <joe.kudyba@lakeviewacademy.org>
2025-03-04 15:30:26 -0500

    Create MrKudybaLearnerCode.java

---
+++ Learning/src/main/java/org/firstinspires/ftc/learning/MrKudybaLearnerCode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.learning;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Mr.Kudyba's TeleOp", group="Starter Code")
+public class MrKudybaLearnerCode extends LinearOpMode {
+
+    @Override
+    public  void  runOpMode() {
+        
+    }
+}
