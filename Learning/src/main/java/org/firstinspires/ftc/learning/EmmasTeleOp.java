e9d92b225adf99187fe1fbaad59c9ff3ef552040
JoeKudyba-LA <joe.kudyba@lakeviewacademy.org>
2025-03-05 11:48:51 -0500

    Practicing for robotics lesson

--- Learning/src/main/java/org/firstinspires/ftc/learning/EmmasTeleOp.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/EmmasTeleOp.java
@@ -5,8 +5,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="Emma's TeleOp", group="Starter Code")
 public class EmmasTeleOp extends LinearOpMode {
+
+    // pressing init starts runOpMode
     @Override
     public void runOpMode() {
 
+        // While init wait for the play button
+        waitForStart();
+
+        // If stop is requested STAWP
+        if (isStopRequested()) return;
+
+        // All the code to make the robot go once we have pressed
+        // play
+        while (opModeIsActive()) {
+
+        }
     }
 }
--- Learning/src/main/java/org/firstinspires/ftc/learning/MrKudybaLearnerCode.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/MrKudybaLearnerCode.java
@@ -2,12 +2,30 @@ package org.firstinspires.ftc.learning;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Mr.Kudyba's TeleOp", group="Starter Code")
 public class MrKudybaLearnerCode extends LinearOpMode {
 
     @Override
+    // This method starts running once we hit init.
     public  void  runOpMode() {
-        
+        //If we want anything to happen before we hit play
+        DcMotor rightMotor = hardwareMap.dcMotor.get("left");
+        DcMotor leftMotor = hardwareMap.dcMotor.get("right");
+
+        //Wait for the driver to press play
+        waitForStart();
+
+        //Check to see if the driver pressed stop, if so STOP.
+        if (isStopRequested()){
+            return;
+        }
+
+        while (opModeIsActive()){
+            //This is all the code we want to do when we press play
+
+        }
+
     }
 }
