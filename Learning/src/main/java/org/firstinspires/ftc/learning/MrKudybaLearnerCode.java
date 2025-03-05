dfaccc26f36b7b1a01ee1403eeba93e6f21e90b2
JoeKudyba-LA <joe.kudyba@lakeviewacademy.org>
2025-03-05 13:06:51 -0500

    Update MrKudybaLearnerCode.java

--- Learning/src/main/java/org/firstinspires/ftc/learning/MrKudybaLearnerCode.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/MrKudybaLearnerCode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.learning;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Mr.Kudyba's TeleOp", group="Starter Code")
 public class MrKudybaLearnerCode extends LinearOpMode {
@@ -11,8 +12,10 @@ public class MrKudybaLearnerCode extends LinearOpMode {
     // This method starts running once we hit init.
     public  void  runOpMode() {
         //If we want anything to happen before we hit play
-        DcMotor rightMotor = hardwareMap.dcMotor.get("left");
-        DcMotor leftMotor = hardwareMap.dcMotor.get("right");
+        DcMotor leftMotor = hardwareMap.dcMotor.get("left");
+        DcMotor rightMotor = hardwareMap.dcMotor.get("right");
+
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //Wait for the driver to press play
         waitForStart();
@@ -24,7 +27,15 @@ public class MrKudybaLearnerCode extends LinearOpMode {
 
         while (opModeIsActive()){
             //This is all the code we want to do when we press play
+            if (gamepad1.left_bumper){
+                leftMotor.setPower(1);
+            }
+            else leftMotor.setPower(0);
 
+            if (gamepad1.right_bumper){
+                rightMotor.setPower(1);
+            }
+            else rightMotor.setPower(0);
         }
 
     }
