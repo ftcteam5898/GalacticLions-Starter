9c1ac37cec0c1b64bd0a09843c76e1e9c174ba9a
Aidahicks101 <aida.hicks@lakeviewacademy.org>
2025-03-06 15:30:04 -0500

    Update AidasTeleOp.java

--- Learning/src/main/java/org/firstinspires/ftc/learning/AidasTeleOp.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/AidasTeleOp.java
@@ -2,11 +2,31 @@ package org.firstinspires.ftc.learning;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="AidasTeleOp", group="Starter Code")
 public class AidasTeleOp extends LinearOpMode {
+    //runOpMode starts once we press init
     @Override
-    public void runOpMode() {
-        
+    public void runOpMode(){
+        //Any code that we write BEFORE waitForStart() will run when
+        //press init but before we press play.
+        DcMotor motorLeft = hardwareMap.dcMotor.get("lob");
+        DcMotor motorRight = hardwareMap.dcMotor.get("rob");
+
+        motorLeft.setPower(0.5);
+
+        //This lets the code wait for us to press the play button
+        waitForStart();
+
+        //If we press stop BEFORE pressing play, stop the code.
+        if (isStopRequested()) return;
+
+        // while (I've hit play())
+        while (opModeIsActive()) {
+           //Do all code here as long as we have pressed play and
+           // not pressed stop.
+            motorRight.setPower(0.5);
+        }
     }
 }
