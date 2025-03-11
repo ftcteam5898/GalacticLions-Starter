960bb219c3a3dc646c61f76a1871df042895c9d3
Micah-Johnson16 <Micah0329@outlook.com>
2025-03-11 15:25:17 -0400

    Update MicahLearnCodeToday.java

--- Learning/src/main/java/org/firstinspires/ftc/learning/MicahLearnCodeToday.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/MicahLearnCodeToday.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Micah's First Code", group="Starter Code")
 public class MicahLearnCodeToday extends LinearOpMode {
@@ -15,6 +16,7 @@ public class MicahLearnCodeToday extends LinearOpMode {
         //press init but before we press play
         DcMotor Pikachu = hardwareMap.dcMotor.get("pokemonGo");
         DcMotor Raichu = hardwareMap.dcMotor.get("pokemonTCGP");
+        Servo Gigalith = hardwareMap.get(Servo.class,"Gigalith");
         Pikachu.setDirection(DcMotorSimple.Direction.REVERSE);
         //this lets the code wait for us to press the play button
         waitForStart();
@@ -22,25 +24,28 @@ public class MicahLearnCodeToday extends LinearOpMode {
         if (isStopRequested()) return;
         //while (I've hit play())
         while (opModeIsActive()) {
-            //do all code here as long as we have pressed play and
-            //not pressed stop
-            //Pikachu.setPower(0); //only way to stop a motor
-            Raichu.setPower(gamepad1.right_trigger);
-            Pikachu.setPower(gamepad1.left_trigger);
-            //IF we press the right bumber do the thing in the { }
-            if (gamepad1.right_bumper){
-                Raichu.setPower(-1);
-            }
-            //IF we let go of right bumber, do something else
-            else {
-                Raichu.setPower(0);
-            }
-            if (gamepad1.left_bumper){
-                Pikachu.setPower(-1);
-            }
-            else {
-                Pikachu.setPower(0);
-            }
+            Pikachu.setPower(-gamepad1.left_stick_y);
+            Raichu.setPower(-gamepad1.right_stick_y);
+            //Trigger & Bumber driving code
+//            //do all code here as long as we have pressed play and
+//            //not pressed stop
+//            //Pikachu.setPower(0); //only way to stop a motor
+//            Raichu.setPower(gamepad1.right_trigger);
+//            Pikachu.setPower(gamepad1.left_trigger);
+//            //IF we press the right bumber do the thing in the { }
+//            if (gamepad1.right_bumper){
+//                Raichu.setPower(-1);
+//            }
+//            //IF we let go of right bumber, do something else
+//            else {
+//                Raichu.setPower(0);
+//            }
+//            if (gamepad1.left_bumper){
+//                Pikachu.setPower(-1);
+//            }
+//            else {
+//                Pikachu.setPower(0);
+//           }
         }
     }
 }
