fcea415d517d4969cce6c638228bff8244c5c4a5
Micah-Johnson16 <Micah0329@outlook.com>
2025-03-13 15:17:12 -0400

    Update MicahLearnCodeToday.java

--- Learning/src/main/java/org/firstinspires/ftc/learning/MicahLearnCodeToday.java
+++ Learning/src/main/java/org/firstinspires/ftc/learning/MicahLearnCodeToday.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.learning;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -16,16 +16,42 @@ public class MicahLearnCodeToday extends LinearOpMode {
         //press init but before we press play
         DcMotor Pikachu = hardwareMap.dcMotor.get("pokemonGo");
         DcMotor Raichu = hardwareMap.dcMotor.get("pokemonTCGP");
+
         Servo Gigalith = hardwareMap.get(Servo.class,"Gigalith");
+        CRServo Boldore = hardwareMap.crservo.get("Boldore");
+
+        //we need to flip the left motor so it goes the same way as the right motor
         Pikachu.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //getting our servo into position before we press start
+        Gigalith.setPosition(0.5);
+
         //this lets the code wait for us to press the play button
         waitForStart();
+
         //if we press stop before pressing play, stop the code
         if (isStopRequested()) return;
         //while (I've hit play())
         while (opModeIsActive()) {
             Pikachu.setPower(-gamepad1.left_stick_y);
             Raichu.setPower(-gamepad1.right_stick_y);
+
+            //controlling the servo with the x axis joystick
+            //Gigalith.setPosition(gamepad1.left_stick_x);
+
+            //controlling the servo with the dpad
+            if (gamepad1.dpad_left){
+                Gigalith.setPosition(0);
+            }
+            else if (gamepad1.dpad_right){
+                Gigalith.setPosition(1);
+            }
+            else Gigalith.setPosition(0.5);
+
+            //controlling the CR servo with the triggers
+            Boldore.setPower(gamepad1.right_trigger);
+            Boldore.setPower(-gamepad1.left_trigger);
+
             //Trigger & Bumber driving code
 //            //do all code here as long as we have pressed play and
 //            //not pressed stop
